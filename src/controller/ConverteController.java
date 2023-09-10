9f515b3ba8c1f696e59b184cda268ca9fab95788
Vini <viniciussilvabarbosa2003@gmail.com>
2023-09-09 23:15:56 -0300

    Comentários
    Comentários adicionados

--- src/controller/ConverteController.java
+++ src/controller/ConverteController.java
@@ -9,21 +9,21 @@ public class ConverteController {
 		String numBinario = "";
 		
 		while (decimal >= 2) {
-			p.push(decimal%2);
-			decimal /= 2;
+			p.push(decimal%2);	//Adiciona o resto da divisao por 2 a pilha 
+			decimal /= 2;	
 		}
-		p.push(decimal);
+		p.push(decimal);	//Adiciona uma ultima vez o valor da divisao do numero por 2
 		
-		int tamanho = p.size();
+		int tamanho = p.size();	//Retorna o tamanho da pilha
 		for (int i = 0; i < tamanho; i++) {
 			try {
-				numBinario += p.pop();
+				numBinario += p.pop();	//Concatena o valor do topo da pilha
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 			
-			return numBinario;
+			return numBinario;	//Retorna o numero binario
 	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -11,14 +11,19 @@ public class Principal {
 
 	public static void main(String[] args) {
 		ConverteController conv = new ConverteController();
-		int x = (int)(Math.random()*1000)+1;
+		int x = (int)(Math.random()*1000)+1;	//Número aleatorio inteiro ate 1000
+		
 		JOptionPane.showMessageDialog(null, "Conversão de número decimal para binário");
+		//Inserçao de um numero para ser convertido
 		int numDecimal = Integer.parseInt(JOptionPane.showInputDialog("Insira um número para a conversão (Máximo: 1000): ", x));
-		while (numDecimal > 1000 || numDecimal < 0) {
+		
+		
+		while (numDecimal > 1000 || numDecimal < 0) {	//Verifica se o numero e valido para ser convertido
 			JOptionPane.showMessageDialog(null, "O número precisa ser menor que 1001 e positivo!");
+			//Proxima tentativa de inclusao de um numero para ser convertido
 			numDecimal = Integer.parseInt(JOptionPane.showInputDialog("Insira um número para a conversão (Máximo: 1000): "));
 		}
-		String binario = conv.decToBin(numDecimal);
+		String binario = conv.decToBin(numDecimal); //Envia o numero inteiro como parametro e recebe sua conversao para binario em String
 		JOptionPane.showMessageDialog(null, "Número convertido para binário: "+binario);
 		
 	}
