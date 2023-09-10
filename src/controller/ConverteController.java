8770ac02db1176042ce7cb29135c4ffd8b470ef9
Vini <viniciussilvabarbosa2003@gmail.com>
2023-09-09 22:50:02 -0300

    Finalização do Método de conversão

--- src/controller/ConverteController.java
+++ src/controller/ConverteController.java
@@ -1,9 +1,29 @@
 package controller;
 
+import model.PilhaInt;
+
 public class ConverteController {
 
 	public String decToBin(int decimal) {
+		PilhaInt p = new PilhaInt();
+		String numBinario = "";
 		
+		while (decimal >= 2) {
+			p.push(decimal%2);
+			decimal /= 2;
+		}
+		p.push(decimal);
+		
+		int tamanho = p.size();
+		for (int i = 0; i < tamanho; i++) {
+			try {
+				numBinario += p.pop();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+			
+			return numBinario;
 	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,8 +1,25 @@
 package view;
 
+import javax.swing.JOptionPane;
+
+import controller.ConverteController;
+
+/*
+ * 	Converte um numero decimal em binario (limitado a 1000)
+ */
 public class Principal {
 
 	public static void main(String[] args) {
+		ConverteController conv = new ConverteController();
+		int x = (int)(Math.random()*1000)+1;
+		JOptionPane.showMessageDialog(null, "Conversão de número decimal para binário");
+		int numDecimal = Integer.parseInt(JOptionPane.showInputDialog("Insira um número para a conversão (Máximo: 1000): ", x));
+		while (numDecimal > 1000 || numDecimal < 0) {
+			JOptionPane.showMessageDialog(null, "O número precisa ser menor que 1001 e positivo!");
+			numDecimal = Integer.parseInt(JOptionPane.showInputDialog("Insira um número para a conversão (Máximo: 1000): "));
+		}
+		String binario = conv.decToBin(numDecimal);
+		JOptionPane.showMessageDialog(null, "Número convertido para binário: "+binario);
 		
 	}
 
